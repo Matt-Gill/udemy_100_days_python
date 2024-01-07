336dc86af4a702ddcfcef1a9881f0ecec1d621f3
Matt-Gill <matthewgill87@googlemail.com>
2024-01-07 17:46:11 +0000

    Update main.py

--- day-1/coding-exercises/lesson-4-day-1-variables/main.py
+++ day-1/coding-exercises/lesson-4-day-1-variables/main.py
@@ -1,1 +1,11 @@
+# There are two variables, a and b from input
+a = input("Input a number: ")
+b = input("Input a number: ")
 
+# Create a third variable to help switch the values
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
