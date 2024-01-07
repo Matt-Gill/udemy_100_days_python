e8141c08d9325a53996775f170ee05edd1da4e97
Matt-Gill <matthewgill87@googlemail.com>
2024-01-07 10:50:33 +0000

    Update main.py

--- day-1/lesson-files/day-1-printing/main.py
+++ day-1/lesson-files/day-1-printing/main.py
@@ -1,3 +1,7 @@
 print("Hello world!")
 print("Hello world!\nHello world!\nHello world!")
 print("Hello" + " " + "Angela")
+
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+print("Hello " + input("What is your name?"))
