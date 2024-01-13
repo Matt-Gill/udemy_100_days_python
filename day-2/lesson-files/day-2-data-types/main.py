d4d7d08bb3fc6170eda85d392e8912f8e60301b5
Matt-Gill <matthewgill87@googlemail.com>
2024-01-13 10:31:50 +0000

    Changing main.py

---
+++ day-2/lesson-files/day-2-data-types/main.py
@@ -0,0 +1,50 @@
+#Data Types
+
+#String
+
+print("Hello"[4])
+
+print("123" + "345")
+
+#Integer
+
+print(123 + 345)
+
+print(123_456_789)
+
+#Float
+
+print(3.14159)
+
+#Boolean
+
+print(True)
+print(False)
+
+num_char = len(input("What is your name?\n"))
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " characters.")
+
+a = float(123)
+print(type(a))
+
+print(70 + float("100.5"))
+print(str(70) + str("100"))
+
+########################
+# Mathematical Operators
+########################
+
+# 3 + 5
+# 7 - 4
+# 3 * 2
+# 6 / 3
+# 2 ** 3
+
+# PEMDASLR
+# ()
+# **
+# * /
+# + -
+
+print(3 * (3 + 3) / 3 - 3)
