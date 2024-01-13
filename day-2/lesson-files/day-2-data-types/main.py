9cf00179699a2ebbd6cd9116ccb3b418f2c59b94
Matt-Gill <matthewgill87@googlemail.com>
2024-01-13 10:59:43 +0000

    Completed lesson code in main.py

--- day-2/lesson-files/day-2-data-types/main.py
+++ day-2/lesson-files/day-2-data-types/main.py
@@ -48,3 +48,25 @@ print(str(70) + str("100"))
 # + -
 
 print(3 * (3 + 3) / 3 - 3)
+
+print(round(8 / 3, 2))
+
+print(8 // 3)
+
+print(type(8 // 3))
+print(type(8 / 3))
+
+result = 4 / 2
+result /= 2
+print(result)
+
+score = 0
+
+# User scores a point
+score += 1
+height = 1.80
+isWinning = True
+print(score)
+
+#f-string
+print(f"your score is {score}, your height is {height}, you are winning {isWinning}")
