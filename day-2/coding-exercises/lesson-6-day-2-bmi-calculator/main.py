1c0ac109239b0d2c1ca752db733be92da1b0656b
Matt-Gill <matthewgill87@googlemail.com>
2024-01-13 10:46:33 +0000

    Completed coding exercise in main.py

---
+++ day-2/coding-exercises/lesson-6-day-2-bmi-calculator/main.py
@@ -0,0 +1,12 @@
+height = input("Enter height in meters as a floating point number e.g: 1.65:\n")
+weight = input("Enter weight in kilograms as an integer e.g: 72:\n")
+# Your code below this line 👇
+weight_as_int = int(weight)
+height_as_float = float(height)
+# Using the exponent operator **
+bmi = weight_as_int / height_as_float ** 2
+# or using multiplication and PEMDAS
+bmi = weight_as_int / (height_as_float * height_as_float)
+
+bmi_as_int = int(bmi)
+print(bmi_as_int)
