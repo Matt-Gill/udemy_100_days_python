705be9aac7c4dda9323db9d0b6e003f8dd34a1fe
Matt-Gill <matthewgill87@googlemail.com>
2024-01-27 10:41:42 +0000

    Adding main.py for bmi-2.0 exercise.

---
+++ day-3/coding-exercises/lesson-9-day-3-bmi-2.0/main.py
@@ -0,0 +1,14 @@
+height = float(input("Enter height in meters as a floating point number e.g: 1.65:\n"))
+weight = int(input("Enter weight in kilograms as an integer e.g: 72:\n"))
+# Your code below this line 👇
+bmi = weight / (height * height)
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")
