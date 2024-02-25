6c1f1d64678a01a611e627ff42e227c31d757435
Matt-Gill <matthewgill87@googlemail.com>
2024-02-25 21:13:40 +0000

    Adusted main.py in banker roulette coding exercise.

--- day-4/coding-exercises/lesson-14-day-4-banker-roulette/main.py
+++ day-4/coding-exercises/lesson-14-day-4-banker-roulette/main.py
@@ -8,4 +8,6 @@ num_items = len(names)
 # Generate random numbers between 0 and the last index. 
 random_choice = random.randint(0, num_items - 1)
 # Choose and print a random name.
-print(f'{names[random_choice]} is going to buy the meal today!')
+person_who_will_pay = names[random_choice]
+
+print(person_who_will_pay + ' is going to buy the meal today!')
