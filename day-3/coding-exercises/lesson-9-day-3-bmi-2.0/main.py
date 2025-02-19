3134ed55a42d75aa658eb66e11f1c726bcbe78cc
Matt-Gill <matthewgill87@googlemail.com>
2025-02-19 23:32:47 +0000

    Adding day-3 updated coding-exercises files.

--- day-3/coding-exercises/lesson-9-day-3-bmi-2.0/main.py
+++ day-3/coding-exercises/lesson-9-day-3-bmi-2.0/main.py
@@ -12,3 +12,18 @@ elif bmi < 35:
   print(f"Your BMI is {bmi}, you are obese.")
 else:
   print(f"Your BMI is {bmi}, you are clinically obese.")
+
+weight = 85
+height = 1.85
+
+bmi = weight / (height ** 2)
+
+# 🚨 Do not modify the values above
+# Write your code below 👇
+
+if bmi < 18.5:
+    print("underweight")
+elif bmi >= 18.5 and bmi < 25:
+    print("normal weight")
+else:
+    print("overweight")
