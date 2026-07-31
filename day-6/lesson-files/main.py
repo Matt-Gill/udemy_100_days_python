114927f266432cd7c00b8ba29c054f11a7d2c856
Matt-Gill <matthewgill87@googlemail.com>
2026-07-31 23:13:22 +0100

    Amending day-6 main.py files

--- day-6/lesson-files/main.py
+++ day-6/lesson-files/main.py
@@ -1,3 +1,14 @@
+# Defining and Calling functions
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
 # Reeborg's World - Square
 def turn_right():
     turn_left()
